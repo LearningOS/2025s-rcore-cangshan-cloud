fdea1a4073d08834c63c78c5df1a357577fcf097
Judai <judai777@outlook.com>
2025-04-17 09:45:17 +0800

    first map

--- os/src/syscall/process.rs
+++ os/src/syscall/process.rs
@@ -1,5 +1,5 @@
 //! Process management syscalls
-use crate::task::{change_program_brk, exit_current_and_run_next, suspend_current_and_run_next};
+use crate::{mm::MapPermission, task::{change_program_brk, current_user_token, exit_current_and_run_next, suspend_current_and_run_next}, timer::get_time_ms};
 
 #[repr(C)]
 #[derive(Debug)]
@@ -27,26 +27,156 @@ pub fn sys_yield() -> isize {
 /// HINT: What if [`TimeVal`] is splitted by two pages ?
 pub fn sys_get_time(_ts: *mut TimeVal, _tz: usize) -> isize {
     trace!("kernel: sys_get_time");
-    -1
+    get_time_ms() as isize
 }
 
 /// TODO: Finish sys_trace to pass testcases
 /// HINT: You might reimplement it with virtual memory management.
-pub fn sys_trace(_trace_request: usize, _id: usize, _data: usize) -> isize {
+pub fn sys_trace(trace_request: usize, id: usize, data: usize) -> isize {
     trace!("kernel: sys_trace");
-    -1
+    let current_task = get_current_task();
+    let token = current_user_token();
+
+    match trace_request {
+        // 功能0，读取当前任务 id 地址处一个字节的无符号整数值
+        0 => {
+            // 尝试读取用户空间的内存
+            let buffers = translated_byte_buffer(token, id as *const u8, 1);
+            if buffers.is_empty() {
+                return -1;
+            }
+            
+            // 检查页表项是否有读权限
+            let page_table = PageTable::from_token(token);
+            let va = VirtAddr::from(id);
+            let vpn = va.floor();
+            match page_table.translate(vpn) {
+                Some(pte) => {
+                    if !pte.is_valid() || !pte.readable() {
+                        return -1;
+                    }
+                    buffers[0][0] as isize
+                }
+                _ => -1,
+            }
+        },
+        
+        // 功能1，写入data到该用户程序id地址处
+        1 => {
+            // 检查页表项是否有写权限
+            let page_table = PageTable::from_token(token);
+            let va = VirtAddr::from(id);
+            let vpn = va.floor();
+            match page_table.translate(vpn) {
+                Some(pte) => {
+                    if !pte.is_valid() || !pte.writable() {
+                        return -1;
+                    }
+                    
+                    // 尝试写入用户空间的内存
+                    let buffers = translated_byte_buffer(token, id as *const u8, 1);
+                    if buffers.is_empty() {
+                        return -1;
+                    }
+                    
+                    // 写入数据
+                    buffers[0][0] = data as u8;
+                    0
+                }
+                _ => -1,
+            }
+        },
+
+        // 功能2，查询当前系统调用次数，本次调用也计入统计
+        2 =>  {
+            if id < MAX_SYSCALL_NUM {
+                get_syscall_counter(current_task, id) as isize
+            } else {
+                -1
+            }
+        },
+
+        _ => -1,
+    }
 }
 
 // YOUR JOB: Implement mmap.
-pub fn sys_mmap(_start: usize, _len: usize, _port: usize) -> isize {
-    trace!("kernel: sys_mmap NOT IMPLEMENTED YET!");
-    -1
+pub fn sys_mmap(start: usize, len: usize, port: usize) -> isize {
+    trace!("kernel: sys_mmap");
+
+    // 1.start按页对齐
+    if start % PAGE_SIZE != 0 {
+        return -1;
+    }
+
+    // 2.prot检查   
+    if prot & 0x7 != 0 {
+        return -1;
+    }
+
+    // 3,无意义的内存
+    if prot & 0x7 == 0 {
+        return -1;
+    }
+
+    // 计算映射长度
+    let len  = if len == 0 {
+        0
+    } else {
+        (len - 1) / PAGE_SIZE + 1
+    } * PAGE_SIZE;
+
+
+    // 获取当前任务
+    let token = current_user_token();
+    let page_table = PageTable::from_token(token);
+
+    //  检查地址区间是否已经被映射
+    let start_vpn = VirtAddr::from(start).floor();
+    let end_vpn = VirtAddr::from(start + len).ceil();
+    for vpn in start_vpn..end_vpn {
+        if page_table.translate(vpn).is_some() {
+            return -1;
+        }
+    }
+
+    // 设置映射权限
+    let mut map_perm = MapPermission::U;
+    if prot & 0x1 != 0 {
+        map_perm |= MapPermission::R;
+    }
+    if prot & 0x2 != 0 {
+        map_perm |= MapPermission::W;
+    }
+    if prot & 0x4 != 0 {
+        map_perm |= MapPermission::X;
+    }
+
+    //  创建新的映射区域
+
 }
 
 // YOUR JOB: Implement munmap.
-pub fn sys_munmap(_start: usize, _len: usize) -> isize {
-    trace!("kernel: sys_munmap NOT IMPLEMENTED YET!");
-    -1
+pub fn sys_munmap(start: usize, len: usize) -> isize {
+    trace!("kernel: sys_munmap");
+
+    // start按页对齐
+    if start % PAGE_SIZE != 0 {
+        return -1;
+    }
+
+    // 计算取消映射长度
+    let len  = if len == 0 {
+        0
+    } else {
+        (len - 1) / PAGE_SIZE + 1
+    } * PAGE_SIZE;
+
+    // 获取当前任务的内存集
+
+    // 检查是否已经被映射
+
+    // 查找并删除映射区域
 }
 /// change data segment size
 pub fn sys_sbrk(size: i32) -> isize {
